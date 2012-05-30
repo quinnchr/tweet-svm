c20264490537a29d3b0d841952813768ad6f791e
Quinn Chrzan <quinnchrzan@gmail.com>
2012-05-30 17:00:16 -0400

    updates to manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 import redis
 import json
-import time
 import inspect
 import subprocess
 
-class manager:
+class CommandError(Exception):
+
+	def __init__(self, value):
+		self.value = value
+
+	def __str__(self):
+		return repr(self.value)
+
+class Manager:
 	
 	def __init__(self):
 		self.functions = dict(inspect.getmembers(self, predicate=inspect.ismethod))
@@ -15,35 +22,44 @@ class manager:
 			command = data[u'command']
 			options = data[u'options']
 			if command in self.functions:
-				print self.functions[command]
-				self.functions[command](**options)
+				code = self.functions[command](**options)
+				if code != 0:
+					raise CommandError(code)
 		except KeyError, ValueError:
 			# bad command, skip it
 			pass
 
 	def add_user(self, **kwargs):
-		user = kwargs[u'user]
-		
+		user = kwargs[u'user']
+		return subprocess.call(['useradd','-r', '-s', '/bin/false', user])
 
 	def add_stream(self, **kwargs):
-		print kwargs
+		user = kwargs[u'user']
+		stream  = kwargs[u'stream']
 
 	def add_source(self, **kwargs):
-		print kwargs
+		user = kwargs[u'user']
+		stream  = kwargs[u'stream']
+		source  = kwargs[u'source']
 
 	def remove_user(self, **kwargs):
-		print kwargs
+		user = kwargs[u'user']
+		code = subprocess.call(['deluser', user])
+		return subprocess.call(['delgroup',user])
 
 	def remove_stream(self, **kwargs):
-		print kwargs
+		user = kwargs[u'user']
+		stream  = kwargs[u'stream']
 
 	def remove_source(self, **kwargs):
-		print kwargs
+		user = kwargs[u'user']
+		stream  = kwargs[u'stream']
+		source  = kwargs[u'source']
 
 if __name__ == '__main__':
 	db = redis.StrictRedis(host='localhost', port=6379, db=0).pubsub()
 	db.subscribe('manage')
 	# start the main loop
-	controller = manager()
+	controller = Manager()
 	for command in db.listen():
 		controller.dispatch(command['data'])
